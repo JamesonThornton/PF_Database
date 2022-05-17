10932d368dcc27fed318a432272c2a0422dda358
JamesonThornton <105732697+JamesonThornton@users.noreply.github.com>
2022-05-17 08:32:00 -0700

    Update EPC Reader.py

--- EPC Reader.py
+++ EPC Reader.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1587]:
-
-
 import pandas as pd
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+import time
+import datetime
+from datetime import date
 
+filepath = r"C:\Users\tiger\Google Drive\PF_DataBase\IEEE 300-Bus System"
+file1 = filepath + "\\" "IEEE300Bus.epc"
 
-# In[1588]:
-
+stop = False
 
 #tables
-data_tables = pd.DataFrame(["bus data",
-"branch data",
+global data_tables0
+data_tables0 = pd.DataFrame(
+["branch data",
+"bus data",
 "transformer data",
 "generator data",
 "load data",
@@ -31,329 +32,156 @@ data_tables = pd.DataFrame(["bus data",
 "gcd data",
 "transaction data",
 "owner data",
-"qtable data"])
+"qtable data",
+"end"])
 
+def df_headers(file1):
+    header_text = pd.DataFrame(index=data_tables0, columns=[0])
+    #read all header rows based on assumed leading string (data tables)
+    #this will also provide the correct order of the data tables found in the file
+    stop = False
+    header_count=0
+    file = open(file1, "r")
+    for step in range(0,1000000):
+        line = file.readline()
+        for item in data_tables0.to_dict(orient='list')[0]:
+            check = re.search(item, line)
+            try:
+                if check.span()[0]==0:
+                    header_text.loc[item]=line
+                    header_count +=1
+                    temp =line
+                    #stop = True
+                    break
+            except:
+                continue
+        if stop:
+            break
+        if(header_count>=len(data_tables0)):
+            break
+    
+    header_text.dropna(inplace=True)
 
-# In[1589]:
+    table_headers = header_text[0].str.split(']', expand=True)[1].str.split(expand=True)
+    return table_headers
 
-
-filepath = r"C:\Users\tiger\Google Drive\PF_DataBase\IEEE 300-Bus System"
-file1 = filepath + "\\" "IEEE300Bus.epc"
-file2 = open(file1, "r")
-
-global linecount
-linecount = 0
-
-
-# In[ ]:
-
-
-
-
-
-# In[1590]:
-
-
-
-header_text = pd.DataFrame(index=data_tables, columns=[0])
-
-
-# In[1591]:
-
-
-data_tables.to_dict(orient='list')[0]
-
-
-# In[1592]:
-
-
-#read all header rows based on assumed leading string (data tables)
-stop = False
-header_count=0
-for step in range(0,100000):
-    linecount +=1
-    line = file2.readline()
-    for item in data_tables.to_dict(orient='list')[0]:
-        check = re.search(item, line)
-        try:
-            if check.span()[0]==0:
-                header_text.loc[item]=line
-                header_count +=1
-                temp =line
-                #stop = True
-                break
-        except:
-            continue
-    if stop:
-        break
-    if(header_count>=len(data_tables)):
-        break
-
-
-# In[1593]:
-
-
-header_text.dropna(inplace=True)
-
-
-# In[1594]:
-
-
-table_headers = header_text[0].str.split(']', expand=True)[1].str.split(expand=True)
-table_headers
-
-
-# In[1595]:
-
-
-def nextline(file, next_table):
-    global stop
-    global linecount
-    line1 = file.readline()
-    linecount +=1
-    check = re.search(next_table, line1)
+def nextline(file, stop_word):
+    stop = False
+    templine = file.readline()
+    check = re.search(stop_word, templine)
     try:
-        if (check.span()[0]==0):
-            stop = True
-            #print('breaking loop')
+        stop = (check.span()[0]==0)
     except:
         pass
+    #repeat for second line, 3rd line, etc. 
+    #'/' denotes continued information on next line
     #repeat for second line
-    if(re.search('/', line1)!=None):
-        line2 = file2.readline()
-        linecount +=1
-        line = line1 + line2
+    if(re.search('/', templine)!=None):
+        line2 = file.readline()
+        line = templine + line2
         #repeat for possible 3rd line
         if(re.search('/', line2)!=None):
-            line3 = file2.readline()
-            linecount +=1
-            line = line1 + line2 + line3
+            line3 = file.readline()
+            line = templine + line2 + line3
             #4th line
             if(re.search('/', line3)!=None):
-                line4 = file2.readline()
-                linecount +=1
-                line = line1 + line2 + line3 + line4
-    else:
-        line = line1
-    line = line.replace('/','').replace('\n','')
+                line4 = file.readline()
+                line = templine + line2 + line3 + line4
+    
+    line = templine.replace('/','').replace('\n','')
     ######
-    temp = re.split('("[^"]*")|\s+|:', line)
+    temp = re.split('("[^"]*")|([[][^]]*])|\s+|:', line)
     temp1 = pd.DataFrame(temp)
     temp2 = temp1.dropna().reset_index(drop=True)
     temp3 = temp2.replace('',np.nan).dropna().reset_index(drop=True)
     line = temp3
     ######
 
-    return line, stop, linecount
-
-
-# # Read Bus Table
-
-# In[1596]:
-
-
-filepath = r"C:\Users\tiger\Google Drive\PF_DataBase\IEEE 300-Bus System"
-file1 = filepath + "\\" "IEEE300Bus.epc"
-file2 = open(file1, "r")
-linecount = 0
-
-
-# In[1597]:
-
-
-#skip headers and get to bus data table
-stop = False
-while not stop:
-    line, stop, linecount = nextline(file2, 'bus data')
-
-BUSD = pd.DataFrame()
-
-#loop to fill bus table
-stop = False
-check= None
-next_table = table_headers.index[1]
-while not stop:
-    line, stop, linecount = nextline(file2,next_table)
-    if stop:
-        break
-    #append model item to aggregate table
-    BUSD = BUSD.append(line.transpose(), ignore_index=True)
-
-new_header = pd.Series(['BUSID', 'BUSNAME', 'BUSKV'] + list(table_headers.loc['bus data']))
-BUSD.rename(columns=new_header.to_dict(), inplace=True)
-
-
-# In[1598]:
-
-
-BUSD
-
-
-# # Read Branch Table
-
-# In[1599]:
-
-
-SECDD =  pd.DataFrame()
-
-#loop to fill table
-stop = False
-check= None
-next_table = table_headers.index[2]
-while not stop:
-    line, stop, linecount = nextline(file2,next_table)
-    if stop:
-        break
-    #append model item to aggregate table
-    SECDD = SECDD.append(line.transpose())
-
-new_header = pd.Series(['FBUS', 'FBUSNAME', 'FBUSKV','TBUS', 'TBUSNAME', 'TBUSKV'] + list(table_headers.loc['branch data']))
-SECDD.rename(columns=new_header.to_dict(), inplace=True)
-
-
-# In[1600]:
-
-
-SECDD
-
-
-# # Read XFMR Table
-
-# In[1601]:
-
-
-line, linecount
-
-
-# In[1602]:
-
-
-XFMR = pd.DataFrame()
-#loop to fill table
-stop = False
-check= None
-next_table = table_headers.index[3]
-while not stop:
-    line, stop, linecount = nextline(file2,next_table)
-    if stop:
-        break
-    #append model item to aggregate table
-    XFMR = XFMR.append(line.transpose())
-
-new_header = pd.Series(['FBUS', 'FBUSNAME', 'FBUSKV','TBUS', 'TBUSNAME', 'TBUSKV'] + list(table_headers.loc['transformer data']))
-XFMR.rename(columns=new_header.to_dict(), inplace=True)   
-XFMR.reset_index(drop=True, inplace=True)
-
-
-# In[1603]:
-
-
-XFMR
-
-
-# # Gen Data
-
-# In[1604]:
-
-
-line
-
-
-# In[1605]:
-
-
-GENS = pd.DataFrame()
-#loop to fill table
-stop = False
-check= None
-next_table = table_headers.index[4]
-while not stop:
-    line, stop, linecount = nextline(file2,next_table)
-    if stop:
-        break
-    #append model item to aggregate table
-    GENS = GENS.append(line.transpose())
-
-new_header = pd.Series(['BUS', 'BUSNAME', 'BUSKV'] + list(table_headers.loc['generator data']))
-GENS.rename(columns=new_header.to_dict(), inplace=True)   
-GENS.reset_index(drop=True, inplace=True)
-
-
-# In[1606]:
-
-
-GENS
-
-
-# # Load Data
-
-# In[1607]:
-
-
-LOAD = pd.DataFrame()
-#loop to fill table
-stop = False
-check= None
-next_table = table_headers.index[5]
-while not stop:
-    line, stop, linecount = nextline(file2,next_table)
-    if stop:
-        break
-    #append model item to aggregate table
-    LOAD = LOAD.append(line.transpose())
-
-new_header = pd.Series(['BUS', 'BUSNAME', 'BUSKV'] + list(table_headers.loc['load data']))
-LOAD.rename(columns=new_header.to_dict(), inplace=True)   
-LOAD.reset_index(drop=True, inplace=True)
-
-
-# In[1608]:
-
-
-LOAD
-
-
-# In[1609]:
-
-
-table_headers.index[5]
-
-
-# # Shunt Data
-
-# In[1610]:
-
-
-SHUNT = pd.DataFrame()
-#loop to fill table
-stop = False
-check= None
-next_table = table_headers.index[6]
-while not stop:
-    line, stop, linecount = nextline(file2,next_table)
-    if stop:
-        break
-    #append model item to aggregate table
-    SHUNT = SHUNT.append(line.transpose())
-
-####################
-###need to figure out Shunt headers####
-##############################
-new_header = pd.Series(['BUS', 'BUSNAME', 'BUSKV'] + list(table_headers.loc['shunt data']))
-SHUNT.rename(columns=new_header.to_dict(), inplace=True)   
-SHUNT.reset_index(drop=True, inplace=True)
-
-
-# In[1611]:
-
-
-SHUNT
-
+    return line, stop
+
+def headerfix(table):
+    if (table=='bus data'):
+        return ['BUS', 'BUSNAME', 'BUSKV', 'TYPE', 'vsched', 'volt', 'angle', 'AREA',
+       'ZONE', 'vmax', 'vmin', 'date_in', 'date_out', 'pid', 'L', 'own', 'st',
+       'latitude', 'longitude', 'island', 'sdmon', 'vmax1', 'vmin1']
+    elif (table == 'branch data'):
+        return ['FBUS', 'FBUSNAME', 'FBUSKV','TBUS', 'TBUSNAME', 'TBUSKV']
+    elif (table == 'transformer data'):
+        return ['FBUS', 'FBUSNAME', 'FBUSKV','TBUS', 'TBUSNAME', 'TBUSKV']
+    elif (table == 'generator data' ):
+        return ['BUS', 'BUSNAME', 'BUSKV', 'ID', 'LONG_ID', 'ST', 'NO', 'REG_NAME', 'REG_KV', 'PRF', 'QRF', 'AR', 'ZONE',
+                   'PGEN', 'PMAX', 'PMIN', 'QGEN', 'QMAX', 'QMIN', 'MBASE', 'CMP_R', 'CMP_X', 'GEN_R', 'GEN_X',
+                   'HBUS', 'HBUS2', 'HBUS3', 'TBUS', 'TBUS2', 'TBUS3', 'DATE_IN', 'DATE_OUT', 'PID', 'N']
+    elif (table == 'load data' ):
+        return ['BUS', 'BUSNAME', 'BUSKV']
+    elif(table=='shunt data'):
+        return ['BUS', 'BUSNAME', 'BUSKV', 'ID', 'TBUS', 'TBUSNAME', 'TBUSKV', 'CKT', 'SEC', 'LONGID', 'ST', 'AR', 'ZONE', 
+                'MW_PU', 'MVAR_PU', 'DATEIN', 'DATEOUT', 'PID', 'N', 
+                'OWN', 'PART1', 'OWN2', 'PART2', 'OWN3', 'PART3', 'OWN4', 'PART4', 
+               'REG_NUM', 'REG_NAME', 'REG_KV', 'M', 'NUM', 'NAME', 'KV', 'ID', 'ST']
+    else:
+        return ['']
+
+def readtable(filename, table):
+    header = pd.DataFrame()
+    table_out = pd.DataFrame()
+    file = open(filename, "r")
+    stop = False
+    #find data table of interest
+    while not stop:
+        line, stop = nextline(file, table)# 'bus data'
+    stop = False
+    check= None
+    # fill header data
+    header = line[3:][0]
+    #determine next key word based on header table order
+    next_indx = data_tables[0][data_tables[0]==table].index[0]+1
+    #read and fill table until next key word
+    while not stop:
+        line, stop = nextline(file,data_tables[0][next_indx])
+        if stop:
+            break
+        #append model item to aggregate table
+        table_out = table_out.append(line.transpose(), ignore_index=True)
+    new_header = pd.Series(headerfix(table) + list(header))
+    table_out.rename(columns=new_header.to_dict(), inplace=True)
+    return table_out
+
+
+start = time.time()
+global data_tables
+temp_tables = df_headers(file1)
+data_tables = pd.DataFrame(temp_tables.index.values)
+BUSD = readtable(file1, 'bus data')
+SECDD = readtable(file1, 'branch data')
+XFMR = readtable(file1, 'transformer data')
+GENS = readtable(file1, 'generator data')
+LOAD = readtable(file1, 'load data')
+SHUNT = readtable(file1, 'shunt data')
+SVD = readtable(file1, "svd data")
+AREA = readtable(file1, "area data")
+ZONE = readtable(file1, "zone data")
+IFACE = readtable(file1, "interface data")
+BFACE = readtable(file1, "interface branch data")
+DCBUS = readtable(file1, "dc bus data")
+DCLINE = readtable(file1, "dc line data")
+DCC = readtable(file1, "dc converter data")
+ZTABLE= readtable(file1, "z table data")
+GCD = readtable(file1, "gcd data")
+TRANS = readtable(file1, "transaction data")
+OWN = readtable(file1, "owner data")
+QTAB = readtable(file1, "qtable data")
+endtime = time.time()-start
+print(endtime)
+
+print("Buses: " + str(BUSD.shape[0]))
+print("Line Sections: " +str(SECDD.shape[0]))
+print("Transformers: " +str(XFMR.shape[0]))
+print("Generators: "+ str(GENS.shape[0]))
+print("Loads: " + str(LOAD.shape[0]))
+print("Shunts: " + str(SHUNT.shape[0]))
 
 # # Write to Excel File
-
-# In[1612]:
-
 
 path = r'C:\Users\tiger\Google Drive\PF_DataBase'
 file1 = 'IEEE_300_Bus.xlsx'
@@ -367,30 +195,3 @@ GENS.to_excel(writer, sheet_name='GENS')
 LOAD.to_excel(writer, sheet_name='LOAD')
 SHUNT.to_excel(writer, sheet_name='SHUNT')
 writer.save()
-
-
-# # Archive
-
-# In[ ]:
-
-
-"bus data"
-"branch data"
-"transformer data"
-"generator data"
-"load data"
-"shunt data"
-"svd data"
-"area data"
-"zone data"
-"interface data"
-"interface branch data"
-"dc bus data"
-"dc line data"
-"dc converter data"
-"z table data"
-"gcd data"
-"transaction data"
-"owner data"
-"qtable data"
-
